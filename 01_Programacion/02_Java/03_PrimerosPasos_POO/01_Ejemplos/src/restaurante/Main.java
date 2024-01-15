c3af0b87e1be86670638343da1fb2a052c3adf67
b4391co <b4391co@proton.me>
2024-01-15 19:23:59 +0100

    Restaurante

---
+++ 01_Programacion/02_Java/03_PrimerosPasos_POO/01_Ejemplos/src/restaurante/Main.java
@@ -0,0 +1,9 @@
+package restaurante;
+
+public class Main {
+    public static void main(String[] args) {
+        Restaurante r1 = new Restaurante(50, 190);
+
+        System.out.println(r1.disponibilidadClientes());
+    }
+}
