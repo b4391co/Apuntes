2f09e0c22be861aa34a73604c4d439e9237e4a8f
b4391co <b4391co@proton.me>
2024-02-29 14:05:48 +0100

    kada

---
+++ 04_Desenvolvemento/01_Tema/04_Dni/dni/src/test/java/edu/gorilas/MainTest.java
@@ -0,0 +1,13 @@
+package edu.gorilas;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class MainTest {
+
+}
