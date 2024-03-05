6d187727fea9415d3520f486f5820247354873d5
b4391co <b4391co@proton.me>
2024-03-05 14:02:18 +0100

    feat(Programacion - Instituto): main

--- 01_Programacion/02_Java/03_PrimerosPasos_POO/04_EjemploExamen/instituto/src/main/java/edu/gorilas/Main.java
+++ 01_Programacion/02_Java/03_PrimerosPasos_POO/04_EjemploExamen/instituto/src/main/java/edu/gorilas/Main.java
@@ -1,10 +1,10 @@
 package edu.gorilas;
 
 import edu.gorilas.especialidad.Especialidad;
+import edu.gorilas.menu.*;
 import edu.gorilas.persona.*;
 
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 
 public class Main {
@@ -12,27 +12,10 @@ public class Main {
     static public ArrayList<Miembro> miembros = new ArrayList<>();
     public static void main( String[] args )
     {
-        rellenarMiembros();
+        Utils.rellenarMiembros();
+        while(true)
+            Menu.menu(miembros);
     }
 
-    public static void mostrarTodos() {
-        for (Miembro miembro : miembros) {
-            System.out.println(miembro.toString());
-        }
-    }
 
-    public static void rellenarMiembros() {
-        Especialidad programacion = new Especialidad(1,"Programacion");
-        Especialidad dwcd = new Especialidad(2,"dwcd");
-        Especialidad dwcs = new Especialidad(3,"dwcs");
-        Especialidad dwcc = new Especialidad(4,"dwcc");
-
-        miembros.add(new Profesor("11111111A","Prof Pepe",50,"Casa Pepe",LocalDate.of(2011,8,2),"Completa",5000,programacion));
-        miembros.add(new Profesor("22222222B","Prof Pepe II",40,"Casa Pepe II",LocalDate.of(2020,3,20),"Parcial",4000,dwcc));
-        miembros.add(new Profesor("33333333P","Prof Pepe III",30,"Casa Pepe V",LocalDate.of(2023,5,3),"Completa",6000,dwcs));
-        miembros.add(new Profesor("44444444P","Prof Pepe IV",20,"Casa Pepe V",LocalDate.of(2017,7,8),"Parcial",1200,dwcd));
-        miembros.add(new Profesor("55555555P","Prof Pepe V",30,"Casa Pepe II",LocalDate.of(2018,1,2),"Completa",5000,programacion));
-
-        miembros.add(new Alumno("12121212A","Pepe X",15,"Casa Pepe",LocalDate.of(2020,6,11),"DAW",7,200));
-    }
 }
