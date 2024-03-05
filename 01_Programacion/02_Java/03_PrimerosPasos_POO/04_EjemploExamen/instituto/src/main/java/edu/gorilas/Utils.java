38b82a2b936b351ee1c800378732e36095f62611
b4391co <b4391co@proton.me>
2024-03-05 18:53:31 +0100

    feat(Programacion - Instituto): utils

--- 01_Programacion/02_Java/03_PrimerosPasos_POO/04_EjemploExamen/instituto/src/main/java/edu/gorilas/Utils.java
+++ 01_Programacion/02_Java/03_PrimerosPasos_POO/04_EjemploExamen/instituto/src/main/java/edu/gorilas/Utils.java
@@ -6,7 +6,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import edu.gorilas.especialidad.Especialidad;
-import edu.gorilas.excepciones.ExcepcionesMenu;
+import edu.gorilas.excepciones.*;
 import edu.gorilas.persona.Alumno;
 import edu.gorilas.persona.Profesor;
 
@@ -18,29 +18,68 @@ public class Utils {
             try {
                 System.out.println("Introduzca un número: ");
                 String num = sc.next();
-                Pattern pat = Pattern.compile("[0-9]");
+                Pattern pat = Pattern.compile("[0-9]{1,20}");
                 match = pat.matcher(num);
                 if (!match.matches())
-                    throw new ExcepcionesMenu(ExcepcionesMenu.NUMERO_INCORRECTO);
+                    throw new Excepciones(Excepciones.NUMERO_INCORRECTO);
                 return Integer.parseInt(num);
-            } catch (ExcepcionesMenu e) {
+            } catch (Excepciones e) {
                 System.out.println("Error: " + e.getMessage());
             }
         } while (true);
     }
-    
+
+    public static double verificarDouble() {
+        Scanner sc = new Scanner(System.in);
+        Matcher match;
+        do {
+            try {
+                System.out.println("Introduzca un número ( double ): ");
+                String num = sc.next();
+                Pattern pat = Pattern.compile("[0-9]{0,9}.[0-9]{0,2}");
+                match = pat.matcher(num);
+                if (!match.matches())
+                    throw new Excepciones(Excepciones.NUMERO_INCORRECTO);
+                return Double.parseDouble(num);
+            } catch (Excepciones e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        } while (true);
+    }
+
+    public static String verificarString() {
+        Scanner sc = new Scanner(System.in);
+        Matcher match;
+        do {
+            try {
+                String s = sc.next();
+                Pattern pat = Pattern.compile("[a-zA-Z]{1,20}");
+                match = pat.matcher(s);
+                if (!match.matches())
+                    throw new Excepciones(Excepciones.TEXTO_INCORRECTA);
+                return s;
+            } catch (Excepciones e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        } while (true);
+    }
+
     public static void rellenarMiembros() {
-        Especialidad programacion = new Especialidad(1,"Programacion");
-        Especialidad dwcd = new Especialidad(2,"dwcd");
-        Especialidad dwcs = new Especialidad(3,"dwcs");
-        Especialidad dwcc = new Especialidad(4,"dwcc");
+        Main.especialidades.add(new Especialidad(1,"Programacion"));
+        Main.especialidades.add(new Especialidad(2,"dwcd"));
+        Main.especialidades.add(new Especialidad(3,"dwcs"));
+        Main.especialidades.add(new Especialidad(4,"dwcc"));
 
-        Main.miembros.add(new Profesor("11111111A","Prof Pepe",50,"Casa Pepe",LocalDate.of(2011,8,2),"Completa",5000,programacion));
-        Main.miembros.add(new Profesor("22222222B","Prof Pepe II",40,"Casa Pepe II",LocalDate.of(2020,3,20),"Parcial",4000,dwcc));
-        Main.miembros.add(new Profesor("33333333P","Prof Pepe III",30,"Casa Pepe V",LocalDate.of(2023,5,3),"Completa",6000,dwcs));
-        Main.miembros.add(new Profesor("44444444P","Prof Pepe IV",20,"Casa Pepe V",LocalDate.of(2017,7,8),"Parcial",1200,dwcd));
-        Main.miembros.add(new Profesor("55555555P","Prof Pepe V",30,"Casa Pepe II",LocalDate.of(2018,1,2),"Completa",5000,programacion));
+        Main.miembros.add(new Profesor("11111111A","Prof Pepe",50,"Casa Pepe",LocalDate.of(2011,8,2),"Completa",5000,Main.especialidades.get(1)));
+        Main.miembros.add(new Profesor("22222222B","Prof Pepe II",40,"Casa Pepe II",LocalDate.of(2020,3,20),"Parcial",4000,Main.especialidades.get(2)));
+        Main.miembros.add(new Profesor("33333333P","Prof Pepe III",30,"Casa Pepe V",LocalDate.of(2023,5,3),"Completa",6000,Main.especialidades.get(3)));
+        Main.miembros.add(new Profesor("44444444P","Prof Pepe IV",20,"Casa Pepe V",LocalDate.of(2017,7,8),"Parcial",1200,Main.especialidades.get(0)));
+        Main.miembros.add(new Profesor("55555555P","Prof Pepe V",30,"Casa Pepe II",LocalDate.of(2018,1,2),"Completa",5000,Main.especialidades.get(3)));
 
-        Main.miembros.add(new Alumno("12121212A","Pepe X",15,"Casa Pepe",LocalDate.of(2020,6,11),"DAW",7,200));
+        Main.miembros.add(new Alumno("12121212A", "Pepe X", 15, "Casa Pepe", LocalDate.of(2020, 6, 11), "DAW", 7, 1200));
+        Main.miembros.add(new Alumno("34343434A","Pepe 3",18,"Casa Pepe",LocalDate.of(2010,6,11),"ASIR",7,2000));
+        Main.miembros.add(new Alumno("45454545A","Pepe 45",20,"Casa Pepe",LocalDate.of(2024,1,11),"DAW",7,500));
+        Main.miembros.add(new Alumno("67676767A","Pepe 7",30,"Casa Pepe",LocalDate.of(2000,6,11),"DAW",7,6200));
+
     }
 }
