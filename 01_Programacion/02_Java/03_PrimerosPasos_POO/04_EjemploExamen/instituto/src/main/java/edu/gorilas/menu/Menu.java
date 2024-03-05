1647e9c46ec502594f478420988a9a8c45036573
b4391co <b4391co@proton.me>
2024-03-05 18:53:44 +0100

    feat(Programacion - Instituto): menu

--- 01_Programacion/02_Java/03_PrimerosPasos_POO/04_EjemploExamen/instituto/src/main/java/edu/gorilas/menu/Menu.java
+++ 01_Programacion/02_Java/03_PrimerosPasos_POO/04_EjemploExamen/instituto/src/main/java/edu/gorilas/menu/Menu.java
@@ -1,34 +1,110 @@
 package edu.gorilas.menu;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
-import javax.rmi.CORBA.Util;
-
+import edu.gorilas.Main;
 import edu.gorilas.Utils;
+import edu.gorilas.excepciones.ExcepcionesMenu;
+import edu.gorilas.persona.Alumno;
+import edu.gorilas.especialidad.*;
 import edu.gorilas.persona.Miembro;
+import edu.gorilas.persona.Profesor;
 
 public class Menu{
-    public static void menu(ArrayList m) {
-        System.out.println("\n\n\n ================================ MENU ================================");
-        System.out.println("( 1 ) Mostrar\t");
+    public static void menu(ArrayList<Miembro> m) {
+        System.out.println("\n\n\n========================= MENU =========================\n");
+        System.out.println("( 1 ) Mostrar\t( 2 ) Alta\t( 3 ) Baja\n");
         switch (Utils.verificarNumero()) {
             case 1:
                 menuMostrar(m);
                 break;
             case 2:
-                
+                alta(m);
+                break;
+            case 3:
+                baja(m);
                 break;
             default:
                 System.out.println("Opcion incorrecta");
                 break;
         }
     }
+    
+    public static void baja(ArrayList<Miembro> m) {
+        System.out.println("\n\n\n========================= MENU =========================\n");
+        System.out.println("( 1 ) Alumnos\t( 2 ) Profesor\n");
+        switch (Utils.verificarNumero()) {
+            case 1:
+                UtilsMenu.mostrarAlumnos(m);
+                break;
+            case 2:
+                UtilsMenu.mostrarProfesores(m);
+                break;
+            default:
+                System.out.println("Opcion incorrecta");
+                break;
+        }
+        System.out.println("\nintroduce un dni para borrar");
+        String dniBorrar = UtilsMenu.verificarDni();
+        for (int i = 0; i < m.size(); i++) {
+            if (m.get(i).getDni().equals(dniBorrar)) {
+                m.remove(i);
+                System.out.println(dniBorrar + " Borrado correctamente");
+            }
+        }
+    }
+
+    public static void alta(ArrayList<Miembro> m) {
+            System.out.println("\n\n\n========================= MENU =========================\n");
+            System.out.println("( 1 ) Alumno\t( 2 ) Profesor\n");
+            int selec = Utils.verificarNumero();
+            String dni = UtilsMenu.verificarDni();
+            System.out.println("nombre :");
+            String nombre = Utils.verificarString();
+            System.out.println("edad :");
+            int edad = Utils.verificarNumero();
+            System.out.println("direccion :");
+            String direccion = Utils.verificarString();
+            System.out.println("año nacimiento :");
+            int anho = UtilsMenu.verificarFecha("[0-9]{4}",ExcepcionesMenu.ANHO_INCORRECTO);
+            System.out.println("mes nacimiento :");
+            int mes = UtilsMenu.verificarFecha("^(?:[0-9]|[1-2][0-9]|12)$",ExcepcionesMenu.MES_INCORRECTO);
+            System.out.println("dia nacimiento :");
+            int dia = UtilsMenu.verificarFecha("(?:[0-9]|[1-2][0-9]|30)",ExcepcionesMenu.DIA_INCORRECTO);
+            LocalDate fechaIncorporacion = LocalDate.of(anho,mes,dia);
+            switch (selec) {
+                case 1:
+                    System.out.println("ciclo :");
+                    String ciclo = Utils.verificarString();
+                    System.out.println("media :");
+                    double media = Utils.verificarDouble();
+                    System.out.println("importe Matricula :");
+                    double importeMatricula = Utils.verificarDouble();
+                    m.add(new Alumno(dni, nombre, edad, direccion, fechaIncorporacion, ciclo, media, importeMatricula));
+                    break;
+                case 2:
+                    System.out.println("jornada (parcial o completa):");
+                    String jornada = Utils.verificarString();
+                    System.out.println("sueldo :");
+                    double sueldo = Utils.verificarDouble();
+                    System.out.println("especialidad ( introduce id ):");
+                    System.out.println(Main.especialidades);
+                    Especialidad espec = Main.especialidades.get(UtilsMenu.verificarEspec() - 1);
+                    m.add(new Profesor(dni, nombre, edad, direccion, fechaIncorporacion, jornada, sueldo, espec));
+                    break;
+                default:
+                    System.out.println("Opcion incorrecta");
+                    break;
+            }
+    }
 
     public static void menuMostrar(ArrayList<Miembro> m) {
-        System.out.println("\n\n\n ================================ MENU ================================");
+        System.out.println("\n\n\n========================= MENU =========================\n");
         System.out.println("( 1 ) Todos\t ( 2 )Alumnos\t( 3 ) Profesores");
-        System.out.println("( 4 ) Edad Media Profesores\t ( 5 ) Nota Media");
+        System.out.println("( 4 ) Edad Media Profesores\t( 5 ) Nota Media\n");
         switch (Utils.verificarNumero()) {
             case 1:
                 UtilsMenu.mostrarTodos(m);
@@ -50,7 +126,6 @@ public class Menu{
                 break;
         }
     }
-    
 }
 
 
--- 01_Programacion/02_Java/03_PrimerosPasos_POO/04_EjemploExamen/instituto/src/main/java/edu/gorilas/menu/UtilsMenu.java
+++ 01_Programacion/02_Java/03_PrimerosPasos_POO/04_EjemploExamen/instituto/src/main/java/edu/gorilas/menu/UtilsMenu.java
@@ -1,48 +1,107 @@
 package edu.gorilas.menu;
 
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import edu.gorilas.excepciones.Excepciones;
+import edu.gorilas.excepciones.ExcepcionesMenu;
 import edu.gorilas.persona.Alumno;
 import edu.gorilas.persona.Miembro;
 import edu.gorilas.persona.Profesor;
 
 public class UtilsMenu {
-
-    @SuppressWarnings("unchecked")
     public static void mostrarTodos(ArrayList<Miembro> miembros) {
+        System.out.println();
         miembros.stream()
                 .map(p -> p.toString())
                 .forEach(System.out::println);
     }
 
-    @SuppressWarnings("unchecked")
     public static void mostrarProfesores(ArrayList<Miembro> miembros) {
+        System.out.println();
         miembros.stream().filter(m -> m instanceof Profesor)
                 .map(p -> p.toString())
                 .forEach(System.out::println);
     }
 
-    @SuppressWarnings("unchecked")
     public static void mostrarAlumnos(ArrayList<Miembro> miembros) {
+        System.out.println();
         miembros.stream().filter(m -> m instanceof Alumno)
                 .map(t -> t.toString())
                 .forEach(System.out::println);
     }
 
     public static void edadMediaProfesores(ArrayList<Miembro> miembros) {
-        System.out.println(miembros.stream()
+
+        System.out.println("\n" + miembros.stream()
                 .filter(m -> m instanceof Profesor)
                 .mapToInt(m -> ((Profesor) m).getEdad())
                 .average()
         );
     }
 
+    public static int verificarFecha(String patron, int ex) {
+        Scanner sc = new Scanner(System.in);
+        Matcher match;
+        do {
+            try {
+                System.out.println("Introduzca una fecha: ");
+                String num = sc.next();
+                Pattern pat = Pattern.compile(patron);
+                match = pat.matcher(num);
+                if (!match.matches())
+                    throw new ExcepcionesMenu(ex);
+                return Integer.parseInt(num);
+            } catch (ExcepcionesMenu e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        } while (true);
+    }
+
     public static void notaMedia(ArrayList<Miembro> miembros, String curso) {
         System.out.println(miembros.stream()
                 .filter(m -> m instanceof Alumno)
                 .mapToDouble(m -> ((Alumno) m).getMedia())
-                .average()
-        );
+                .average());
     }
 
+    public static String verificarDni() {
+        Scanner sc = new Scanner(System.in);
+        Matcher match;
+        do {
+            try {
+                System.out.println("Introduzca un dni: ");
+                String dni = sc.next();
+                Pattern pat = Pattern.compile("[0-9]{8}[A-Z]{1}");
+                match = pat.matcher(dni);
+                if (dni.length() != 9)
+                    throw new ExcepcionesMenu(ExcepcionesMenu.DNI_LENGHT);
+                if (!match.matches())
+                    throw new ExcepcionesMenu(ExcepcionesMenu.DNI_INCORRECTO);
+                return dni;
+            } catch (ExcepcionesMenu e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        } while (true);
+    }
+    
+    public static int verificarEspec() {
+        Scanner sc = new Scanner(System.in);
+        Matcher match;
+        do {
+            try {
+                System.out.println("Introduzca un número: ");
+                String num = sc.next();
+                Pattern pat = Pattern.compile("[0-4]{1}");
+                match = pat.matcher(num);
+                if (!match.matches())
+                    throw new ExcepcionesMenu(ExcepcionesMenu.ESPECIALIDAD_INCORRECTA);
+                return Integer.parseInt(num);
+            } catch (ExcepcionesMenu e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        } while (true);
+    }
 }
