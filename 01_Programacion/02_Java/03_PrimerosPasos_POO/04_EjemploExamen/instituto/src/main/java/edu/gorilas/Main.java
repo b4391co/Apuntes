21acb2d4618575a2e4d92bdcc0eecdc0e0bf505a
b4391co <b4391co@proton.me>
2024-03-05 18:53:25 +0100

    feat(Programacion - Instituto): main

--- 01_Programacion/02_Java/03_PrimerosPasos_POO/04_EjemploExamen/instituto/src/main/java/edu/gorilas/Main.java
+++ 01_Programacion/02_Java/03_PrimerosPasos_POO/04_EjemploExamen/instituto/src/main/java/edu/gorilas/Main.java
@@ -4,18 +4,16 @@ import edu.gorilas.especialidad.Especialidad;
 import edu.gorilas.menu.*;
 import edu.gorilas.persona.*;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Main {
     
     static public ArrayList<Miembro> miembros = new ArrayList<>();
+    static public ArrayList<Especialidad> especialidades = new ArrayList<>();
     public static void main( String[] args )
     {
         Utils.rellenarMiembros();
         while(true)
             Menu.menu(miembros);
     }
-
-
 }
