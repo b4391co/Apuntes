d8b494fd9e1478b6288ccdba4b9ca9f1711fcc05
b4391co <b4391co@proton.me>
2024-03-06 18:29:49 +0100

    refactor(DWCS): Tarea

---
+++ 02_Contorno-Servidor/06_AJAX/06_Tarea/app/js/global.js
@@ -0,0 +1,14 @@
+
+const base_url = "http://localhost/controller/FrontController.php";
+
+window.onload = onceLoaded;
+
+
+function onceLoaded() {
+
+    console.debug("window loaded");
+    document.querySelector('#formLogin').onsubmit = login;
+    
+    getRoles();
+}
+
