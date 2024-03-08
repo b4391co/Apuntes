c030cb719f91f95da4fd5c64f345491752064ae0
b4391co <b4391co@proton.me>
2024-03-08 12:47:38 +0100

    fix(Vehicle): id

--- 04_Desenvolvemento/05_intercepting/intercepting/src/main/java/edu/gorilas/targets/Vehicle.java
+++ 04_Desenvolvemento/05_intercepting/intercepting/src/main/java/edu/gorilas/targets/Vehicle.java
@@ -2,7 +2,7 @@ package edu.gorilas.targets;
 
 public class Vehicle implements Target {
     @Override
-    public void ejecucion(String missatge) {
+    public void ejecucion(String id) {
         System.out.println("Puerta abierta " + id.toString() + "!");
     }
     
