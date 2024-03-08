7fa341b857105dbbe9b077879f8dcb5667789395
b4391co <b4391co@proton.me>
2024-03-08 12:49:24 +0100

    refactor(App): OK

--- 04_Desenvolvemento/05_intercepting/intercepting/src/main/java/edu/gorilas/App.java
+++ 04_Desenvolvemento/05_intercepting/intercepting/src/main/java/edu/gorilas/App.java
@@ -1,10 +1,12 @@
 package edu.gorilas;
 
-// import edu.gorilas.administrador.ProgramadorTasques;
-// import edu.gorilas.clients.Mollapp;
-// import edu.gorilas.filtres.Autoritzacio;
-// import edu.gorilas.targets.Vehicle;
-// import edu.gorilas.filtres.Autenticacio;
+import edu.gorilas.administrador.ProgramadorTareas;
+
+import edu.gorilas.administrador.ProgramadorTareas;
+import edu.gorilas.clientes.Mollapp;
+import edu.gorilas.filtros.Autorizacion;
+import edu.gorilas.targets.Vehicle;
+import edu.gorilas.filtros.Autenticacion;
 
 public final class App {
     public static void main(String[] args) {
@@ -15,22 +17,22 @@ public final class App {
          * desde vehiculos a cualquier cosa que admita
          * la recepcion de un mensaje.
          */
-        //ProgramadorTasques programadorTasques = new ProgramadorTasques(new Vehicle());
+        ProgramadorTareas ProgramadorTareas = new ProgramadorTareas(new Vehicle());
 
         /**
          * Añadir al sistema las tareas que queremos que el sistema
          * ejecute al recibir la peticion del cliente.
          */
-        //programadorTasques.setTasca(new Autenticacio());
-        //programadorTasques.setTasca(new Autoritzacio());
+        ProgramadorTareas.setTarea(new Autorizacion());
+        ProgramadorTareas.setTarea(new Autorizacion());
 
         /**
          * Configuracion de la app cliente para que
          * ejecute las tareas programades y
          * envie el mensaje al sistema.
          */
-        //Mollapp mollapp = new Mollapp();
-        //mollapp.setProgramadorTasques(programadorTasques);
-        //mollapp.enviarPeticio("Francesc");
+        Mollapp mollapp = new Mollapp();
+        mollapp.setProgramadorTareas(ProgramadorTareas);
+        mollapp.enviarPeticion("Francesc");
     }
 }
