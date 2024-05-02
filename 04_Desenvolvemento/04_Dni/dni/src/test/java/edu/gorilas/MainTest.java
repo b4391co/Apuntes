5de616ddd99619883ff111f913ba6458eb74c8a6
b4391co <b4391co@proton.me>
2024-05-02 16:39:50 +0200

    fixes

--- 04_Desenvolvemento/04_Dni/dni/src/test/java/edu/gorilas/MainTest.java
+++ 04_Desenvolvemento/04_Dni/dni/src/test/java/edu/gorilas/MainTest.java
@@ -1,9 +1,5 @@
 package edu.gorilas;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 
 /**
  * Unit test for simple App.
